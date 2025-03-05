3b39ecfc6dffaa164fc4e9d7ee480bd52c196049
NymQHDT <lavergnemarianne@gmail.com>
2025-03-05 16:29:22 +0100

    commit init

---
+++ src/main/java/com/diversBrain/ProjetDiversBrainApplication.java
@@ -0,0 +1,13 @@
+package com.diversBrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDiversBrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetDiversBrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diversBrain/ProjetDiversBrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diversBrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetDiversBrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
